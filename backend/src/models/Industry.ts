30ad952602d9cfb2b92c5d8344788716072cd0c0
fadihamad40984 <fadihamad40984@gmail.com>
2025-11-26 11:25:52 +0200

    Feature(backend): add Industry model

---
+++ backend/src/models/Industry.ts
@@ -0,0 +1,42 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+/**
+ * Industry Interface
+ */
+export interface IIndustry extends Document {
+  _id: mongoose.Types.ObjectId;
+  name: string;
+  isActive: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+/**
+ * Industry Schema
+ */
+const IndustrySchema = new Schema<IIndustry>(
+  {
+    name: {
+      type: String,
+      required: [true, 'Industry name is required'],
+      unique: true,
+      trim: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+/**
+ * Indexes
+ * Note: name index is already created by unique: true constraint
+ */
+
+const Industry = mongoose.model<IIndustry>('Industry', IndustrySchema);
+
+export default Industry;
