596c9373aaeb9ec05c4a6a0730641d0d53cde6ab
fadihamad40984 <fadihamad40984@gmail.com>
2025-11-26 11:25:52 +0200

    Feature(backend): add City model

---
+++ backend/src/models/City.ts
@@ -0,0 +1,49 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+/**
+ * City Interface
+ */
+export interface ICity extends Document {
+  _id: mongoose.Types.ObjectId;
+  name: string;
+  country: string;
+  isActive: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+/**
+ * City Schema
+ */
+const CitySchema = new Schema<ICity>(
+  {
+    name: {
+      type: String,
+      required: [true, 'City name is required'],
+      unique: true,
+      trim: true,
+    },
+    country: {
+      type: String,
+      required: [true, 'Country is required'],
+      default: 'Palestine',
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+/**
+ * Indexes
+ * Note: name index is already created by unique: true constraint
+ */
+CitySchema.index({ country: 1 });
+
+const City = mongoose.model<ICity>('City', CitySchema);
+
+export default City;
