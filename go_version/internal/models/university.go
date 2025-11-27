24130d5ceedd139f104adc207ca90b374c1ac508
Omar Jarbou <omarjarbou2019@gmail.com>
2025-11-27 16:36:46 +0200

    Add University model with MongoDB integration, including fields for ID, name, country, city, active status, and timestamps.

--- go_version/internal/models/university.go
+++ go_version/internal/models/university.go
@@ -1,1 +1,14 @@
 package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type University struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	Name      string             `bson:"name,omitempty"`
+	Country   string             `bson:"country,omitempty"`
+	City      string             `bson:"city,omitempty"`
+	IsActive  bool               `bson:"isActive,omitempty"`
+	CreatedAt primitive.DateTime `bson:"createdAt,omitempty"`
+	UpdatedAt primitive.DateTime `bson:"updatedAt,omitempty"`
+	Version   int32              `bson:"__v,omitempty"`
+}
