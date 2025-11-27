ab99b0d2353391a463bd33abf06f4c7148d47317
Omar Jarbou <omarjarbou2019@gmail.com>
2025-11-27 16:48:58 +0200

    Add Major model with MongoDB integration, including fields for ID, name, category, active status, and timestamps.

--- go_version/internal/models/major.go
+++ go_version/internal/models/major.go
@@ -1,1 +1,13 @@
 package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Major struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	Name      string             `bson:"name,omitempty"`
+	Category  string             `bson:"category,omitempty"`
+	IsActive  bool               `bson:"isActive,omitempty"`
+	CreatedAt primitive.DateTime `bson:"createdAt,omitempty"`
+	UpdatedAt primitive.DateTime `bson:"updatedAt,omitempty"`
+	Version   int32              `bson:"__v,omitempty"`
+}
