b612c3cd8f09205b327c3c01d490bc24cb7abab3
Omar Jarbou <omarjarbou2019@gmail.com>
2025-11-27 16:43:41 +0200

    Add City model with MongoDB integration, including fields for ID, name, country, active status, and timestamps.

--- go_version/internal/models/city.go
+++ go_version/internal/models/city.go
@@ -1,1 +1,13 @@
 package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type City struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	Name      string             `bson:"name,omitempty"`
+	Country   string             `bson:"country,omitempty"`
+	IsActive  bool               `bson:"isActive,omitempty"`
+	CreatedAt primitive.DateTime `bson:"createdAt,omitempty"`
+	UpdatedAt primitive.DateTime `bson:"updatedAt,omitempty"`
+	Version   int32              `bson:"__v,omitempty"`
+}
